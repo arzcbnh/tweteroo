1337a50536105596a1cc609faa73f7d8f62ab35a
Matheus Henrique <matthpmoreira@gmail.com>
2024-09-20 00:59:59 -0300

    feat: run app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express, { json } from "express";
+import cors from "cors";
+
+const port = process.env.PORT;
+const app = express();
+app.use(json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Running");
+});
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+})
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "dotenv/config";
+import "./app.js";
+
+import joi from "joi";
